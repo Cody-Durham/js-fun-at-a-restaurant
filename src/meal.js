a43ba88449366d4cd2206d5d9cf24925cbdfdee8
Cody Durham <codydd@gmail.com>
2020-10-10 18:15:16 -0600

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  return `Delicious ${food}`;
 }
-
+// -------------------------------------------
+function createMenuItem(name, price, type) {
+  var menuItem = {
+  name: name,
+  price: price,
+  type: type,
+};
+  return menuItem
+};
+// -------------------------------------------
+function addIngredients(topping, ingredients) {
+  if (ingredients.includes(topping)) {
+    return ingredients
+  }
+  ingredients.push(topping);
+};
+// -------------------------------------------
+function formatPrice(initialPrice) {
+  console.log(initialPrice);
+  return `$${initialPrice}`
+};
+// -------------------------------------------
+//Need to come back and make this more dynamic--
+function decreasePrice(price) {
+  return price * .9
+};
+// -------------------------------------------
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  };
+  return recipe
+}
+// -------------------------------------------
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
