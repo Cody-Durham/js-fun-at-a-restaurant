8e18362dd3f1600667984e06e4c4f53c7acae563
Cody Durham <codydd@gmail.com>
2020-10-11 18:16:35 -0600

    Initial commit

--- src/order.js
+++ src/order.js
@@ -1,7 +1,69 @@
 
+function takeOrder(newOrder, deliveryOrders) {
+
+if (deliveryOrders.length < 3) {
+  deliveryOrders.push(newOrder)
+}
+};
+//-------------------------------------------------------
+// function refundOrder(order, deliveryOrders) {
+//
+//   for (var i = 0; i < deliveryOrders.length ; i++) {
+//
+//     if (deliveryOrders[i].orderNumber === order) {
+//       deliveryOrders.shift(deliveryOrders)
+//     }
+//   }
+// };
+
+function refundOrder(order, deliveryOrders) {
+  var removeOrder = deliveryOrders.splice(order -1, 1);
+  return removeOrder
+  };
+//-------------------------------------------------------
+function listItems(deliveryOrders) {
+  var newItemList = []
+
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    newItemList.push(deliveryOrders[i].item);
+    }
+      return newItemList.join(', ')
+  };
+//-------------------------------------------------------
+
+function searchOrder(deliveryOrders) {
+
+  for (var i = 0; i < deliveryOrders; i++) {
+    if (deliveryOrders[i].item === 'burger') {
+    }
+  }
+};
+
+
+
+//---------------------------  ^
+order1.orderNumber
+// order2.orderNumber
+// order3.orderNumber
+// this is the the iteration how the [i] works.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
